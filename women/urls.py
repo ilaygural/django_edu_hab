d6f860d92d4336caefee6d9836e62956fb01c324
ilaygural <ilaygural@gmail.com>
2025-10-02 23:33:01 +0700

    Реализованы варианты подключения маршрутов - path, re_path, convertters

--- women/urls.py
+++ women/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index), # http://127.0.0.1:8000/women/
-    path('cats/', views.categories),
+    path('', views.index),  # http://127.0.0.1:8000/women/
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive),
+    path('archive/<year4:year>/', views.archive),
 ]
