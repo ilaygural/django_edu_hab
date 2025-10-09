c0c864134a11ba16fb2314819475fa83d9034c3a
ilaygural <ilaygural@gmail.com>
2025-10-09 23:03:46 +0700

    Реализованы шаблоны с различными типами данных python

--- schedule/views.py
+++ schedule/views.py
@@ -4,7 +4,17 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'schedule/index.html')
+    data = {
+        'title': '📅 Edu_Hub - Расписание',
+        'description': 'Система управления учебным процессом',
+        'features': ['Курсы', 'Уроки', 'Преподаватели', 'Студенты'],
+        'stats': {
+            'total_courses': 15,
+            'active_teachers': 8,
+            'students_count': 120
+        }
+    }
+    return render(request, 'schedule/index.html', data)
 
 
 def courses_list(request):
