f158deed727aaaa5f3b80a8726a7b5bbd203aad0
ilaygural <ilaygural@gmail.com>
2025-10-12 21:51:46 +0700

    Добавлен статик оформление, убраны лишние учебные строки кода

--- women/views.py
+++ women/views.py
@@ -12,7 +12,9 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         ]
 
 data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
+    Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
+     'is_published': True},
     {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
@@ -29,14 +31,18 @@ def index(request):
     }
     return render(request, 'women/index.html', context=data)
 
+
 def addpage(request):
     return HttpResponse('Добавление статьи')
+
 
 def contact(request):
     return HttpResponse('Обратная связь')
 
+
 def login(request):
     return HttpResponse('Авторизация')
+
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
