192118d29519b221c5b7584a7630049adf59d55f
И. С. Гураль <-emailaygural@gmail.com>
2025-11-18 16:42:43 +0700

    Доваблено отображение тегов

--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -1,6 +1,6 @@
 from django import template
 import women.views as views
-from women.models import Category
+from women.models import Category, TagPost
 
 register = template.Library()
 
@@ -8,3 +8,8 @@ register = template.Library()
 def show_categories(cat_selected_id=0):
     cats = Category.objects.all()
     return {"cats": cats, "cat_selected": cat_selected_id}
+
+
+@register.inclusion_tag('women/list_tags.html')
+def show_all_tags():
+    return {"tags": TagPost.objects.all()}
